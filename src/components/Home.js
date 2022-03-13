e87dee11e1a78a89580825186d423c4e428ae229
vijaySai-22 <vijaysaiprasadpujari@gmail.com>
2022-03-13 12:06:45 +0530

    contact layout created

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,10 +14,34 @@ export default function Home() {
     const signout=()=>{
         signOut(auth)
     }
+
+    const[data,setData]=useState({
+        contactName:'',
+        contactNumber:'',
+        contactMail:'',
+    })
+    const {contactName,contactNumber,contactMail}=data
+    const change=(e)=>{
+        setData({...data,[e.target.name]:e.target.value})
+    }
+    const addContact=(e)=>{
+        e.preventDefault();
+        console.log(data)
+    }
   return (
     <div>
-        {name}
         <button onClick={signout}>Signout</button>
+        <h1>Contact Form</h1>
+        <h2>Add Contacts</h2>
+        <h3>Name</h3>
+        <input placeholder='Name' name='contactName' value={contactName} onChange={change} />
+        <h3>Ph.No</h3>
+        <input placeholder='Phone no' name='contactNumber' value={contactNumber} onChange={change} />
+        <h3>Email</h3>
+        <input placeholder='Email' name='contactMail' value={contactMail} onChange={change} />
+        <button onClick={addContact}>Save</button>
+        <h1>My Contacts</h1>
+
     </div>
   )
 }
