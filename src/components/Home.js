48215a7fd209c0342a58ff13004afe2bdc851664
vijaySai-22 <vijaysaiprasadpujari@gmail.com>
2022-03-13 18:48:16 +0530

    updated - restricted to push blank data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
 import { onAuthStateChanged, signOut } from '@firebase/auth'
-import { addDoc, collection } from '@firebase/firestore'
+import { addDoc, collection, getDocs, } from '@firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { auth, db } from '../firebase'
-import Contactlist from './Contactlist'
 
 export default function Home() {
     const [id,setId] = useState()
@@ -28,19 +27,37 @@ export default function Home() {
     }
     async function addContact(e){
         e.preventDefault();
-        console.log(data)
-        try {
-            const docRef = await addDoc(collection(db, 'contacts'), {
-              cid:id,
-              cname: contactName,
-              cnumber: contactNumber,
-              cmail: contactMail
-            });
-            console.log("Document written with ID: ", docRef.id);
-        }catch (e) {
-            console.error("Error adding document: ", e);
+        if(contactMail!==''&&contactNumber!==""&&contactMail!==''){
+            try {
+                const docRef = await addDoc(collection(db, 'contacts'), {
+                  cid:id,
+                  cname: contactName,
+                  cnumber: contactNumber,
+                  cmail: contactMail
+                });
+                console.log("Document written with ID: ", docRef.id);
+            }catch (e) {
+                console.error("Error adding document: ", e);
+            }
         }
+        
     }
+    const [contactList,setContactList] = useState([])
+    const usersContacts=collection(db,'contacts');
+    useEffect(()=>{
+        const getData=async()=>{
+            await getDocs(usersContacts)
+            .then((snapshot)=>{
+                let contacts = []
+                snapshot.docs.forEach((doc)=>{
+                    contacts.push({...doc.data(),id:doc.id})
+                })
+                setContactList(contacts)
+            })
+            
+        }
+        getData()
+    },[addContact])
 
   return (
     <div>
@@ -48,14 +65,30 @@ export default function Home() {
         <h1>Contact Form</h1>
         <h2>Add Contacts</h2>
         <h3>Name</h3>
-        <input placeholder='Name' name='contactName' value={contactName} onChange={change} />
+        <input placeholder='Name' name='contactName' value={contactName} onChange={change} required/>
         <h3>Ph.No</h3>
-        <input placeholder='Phone no' name='contactNumber' value={contactNumber} onChange={change} />
+        <input placeholder='Phone no' name='contactNumber' value={contactNumber} onChange={change} required/>
         <h3>Email</h3>
-        <input placeholder='Email' name='contactMail' value={contactMail} onChange={change} />
+        <input placeholder='Email' name='contactMail' type='email' value={contactMail} onChange={change} required />
         <button onClick={addContact}>Save</button>
         <h1>My Contacts</h1>
-        <Contactlist id={id}/>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Ph no</th>
+                <th>Mail</th>
+            </tr>
+            {contactList.map((e) => {
+                if(e.cid===id)
+            return (
+                <tr key={e.id}>
+                    <td>{e.cname}</td>
+                    <td>{e.cnumber}</td>
+                    <td>{e.cmail}</td>
+                </tr>
+            )
+            })}
+        </table>
     </div>
   )
 }
